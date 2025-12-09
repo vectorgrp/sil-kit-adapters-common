b29a7e4f71f9e190bf491bfd5ab5ac9afbe58e93
Al Najar, Ismaeel <Ismaeel.AlNajar@vector.com>
2025-12-09 11:53:19 +0100

    SKA-962: Use SIL Kit 5.0.2 (#33)

--- cmake/FindSilKit.cmake
+++ cmake/FindSilKit.cmake
@@ -14,7 +14,7 @@
 # Required minimal SIL Kit version during lookup.
 set(SILKIT_REQUIRED_MIN_VERSION "4.0.40")
 
-set(SILKIT_DEFAULT_DOWNLOAD_VERSION "5.0.1" CACHE STRING "If no SIL Kit package is specified with SILKIT_VERSION, this package version will be downloaded")
+set(SILKIT_DEFAULT_DOWNLOAD_VERSION "5.0.2" CACHE STRING "If no SIL Kit package is specified with SILKIT_VERSION, this package version will be downloaded")
 set(SILKIT_DEFAULT_DOWNLOAD_FLAVOR_NONWIN "ubuntu-22.04-x86_64-gcc" CACHE STRING "If no SIL Kit package is specified, this package flavor will be downloaded on non-windows system.")
 set(SILKIT_DEFAULT_DOWNLOAD_FLAVOR_WIN "Win-x86_64-VS2019" CACHE STRING "If no SIL Kit package is specified, this package flavor will be downloaded on Windows systems.")
 
